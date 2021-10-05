216315c68031c84e16a9a0baebee992a905e0729
rickferreiramaia <riclecioskatesk8@gmail.com>
2021-10-04 21:13:14 -0300

    ExerciciosAulaDeVariaveis

---
+++ modulo1/variaveis/index1.js
@@ -0,0 +1,14 @@
+
+
+let jornadaDeTrabalho= prompt("Quantas horas você trabalha por dia?")
+let Salario = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${Salario/jornadaDeTrabalho} por hora`)   
+
+console.log('jornadaDeTrabalho',jornadaDeTrabalho)
+console.log('salario',Salario)
+console.log("voce recebe por hora",Salario/jornadaDeTrabalho )
+
+
+//let aluno
+//let idade
+//undefined - por que não atribuiu nem um valor sem valor definido
---
+++ modulo1/variaveis/index3.js
@@ -0,0 +1,31 @@
+// Exercicio 2
+
+let gostaDeSorvetes = prompt('Gosta de Sorvete?')
+console.log('Voce gosta de sorvete?',gostaDeSorvetes)
+
+alert(`Voce gosta de Sorvete? - ${gostaDeSorvetes} `)
+
+let gostaDeMacarrao = prompt('Gosta de Macarrão?')
+console.log('Voce gosta de macarrão?',gostaDeMacarrao)
+
+alert(`Voce gosta de macarrão? - ${gostaDeMacarrao} `)
+
+
+let gostaDeBatata = prompt('Gosta de Batata Frita?')
+console.log('Voce gosta de batata frita?',gostaDeBatata)
+
+alert(`Voce gosta de Batata Frita? - ${gostaDeBatata} `)
+
+// Exercicio 3
+
+let a = 10
+let b = 5
+let c = a
+a = b
+b = c
+console.log("O novo valor de a é", a) 
+console.log("O novo valor de b é", b)
+
+
+
+
